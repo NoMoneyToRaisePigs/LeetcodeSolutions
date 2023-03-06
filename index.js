a516e6594534b41305ba17c22bf58e24809fcb37
NoMoneyToRaisePigs <gaofan19901103@gmail.com>
2023-03-06 22:46:36 +0800

    DOM - ts transpile

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function handleClick() {
 
 const throttledHandelClick = _throttle(handleClick, 2000)
 const debounceddHandelClick = _debounce(handleClick, 2000)
+
+
+
+// -----mimic vue _openBlock
+function xxxx() { 
+   return (a(), b())
+}
+
+function a() {
+   console.log('func a')
+   return 'a'
+}
+function b() { 
+   console.log('func b')
+   return 'b'
+}
+
+
+xxxx()
