3c4518c5a5130279b90d8f4f84e9b31c98d05fb8
NoMoneyToRaisePigs <gaofan19901103@gmail.com>
2023-03-23 00:04:51 +0800

    Array - update Valid Sudoku

--- Algorithm/Array/src/ValidSudoku.js
+++ Algorithm/Array/src/ValidSudoku.js
@@ -20,21 +20,27 @@ export default function isValidSudoku(board) {
    }
    
        //check 3 * 3
-       for(let i = 3; i < 9; i += 3){
-           const SudokuMap = {}
+      for (let i = 1; i < 9; i += 3){
+         for (let j = 1; j < 9; j += 3) { 
+
+            const SudokuMap = {}
    
-           for(let row = 0; row < i; row++){
-               for(let column = 0; column < i; column++){
-                   const numStr = SudokuMap[board[row][column]]
-   
-                   if(isDuplicate(numStr, SudokuMap)){
-                       return false
-                   } else {
-                       SudokuMap[numStr] = true
-                   }
-               }
-           }
-       }
+            for(let row = i - 1; row < i - 1 + 3; row++){
+                for(let column =  j - 1; column < j - 1 + 3; column++){
+                    const numStr = board[row][column]
+                  //  console.log(numStr)
+                   
+                    if(isDuplicate(numStr, SudokuMap)){
+                        return false
+                    } else {
+                       if(numStr != '.')
+                         SudokuMap[numStr] = true
+                    }
+                }
+            }
+
+         }
+      }
    
        //check row
        for(let i = 0; i < board.length; i++){
@@ -44,7 +50,8 @@ export default function isValidSudoku(board) {
                if(isDuplicate(board[i][j], map)){
                    return false
                } else {
-                   map[board[i][j]] = true
+                  if(board[i][j] != '.')
+                     map[board[i][j]] = true
                }
            }
        }
@@ -55,10 +62,13 @@ export default function isValidSudoku(board) {
    
            for(let j = 0; j < board[i].length; j++){
    
-               if(isDuplicate(board[j][i], map)){
+               const numStr = board[j][i]
+              
+               if(isDuplicate(numStr, map)){
                    return false
                } else {
-                   map[board[j][i]] = true
+                  if(numStr != '.')
+                     map[numStr] = true
                }
            }
        }
